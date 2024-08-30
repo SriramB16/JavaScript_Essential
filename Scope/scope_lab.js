f9f7891bd0d551beb72030485ff04cccc6b1274d
SriramB16 <sridhanu204@gmail.com>
2024-08-30 08:00:15 -0400

    formatted

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -22,7 +22,10 @@ function show(){
     let functionLet = "I'm a block-scoped let";
     const functionConst = "I'm a block-scoped const";
     }
-    show();
-    console.log(functionVar); // Throws ReferenceError
-    console.log(functionLet); // Throws ReferenceError
-    console.log(functionConst); // Throws ReferenceError
+
+show();
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
+
+
