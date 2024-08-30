c588e3e0d1b885a76ea2eee12a317a57860f1013
SriramB16 <sridhanu2004@gmail.com>
2024-08-30 11:34:23 -0400

    some changes made in control_flow.js file

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -47,3 +47,11 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
