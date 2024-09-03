5b9ada7c925a14bc1065317445bf5f830136c641
SriramB16 <sridhanu2004@gmail.com>
2024-09-02 23:58:51 -0400

    followers count counter code is created in new file

---
+++ followersCount/followers_count.js
@@ -0,0 +1,24 @@
+let count = 0;
+
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount() {
+    document.getElementById("countDisplay").innerHTML = count
+}
+
+function checkCountValue() {
+    if ( count === 10 ) {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if ( count === 20 ) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
+
+function resetCount() {
+    count = 0;
+    document.getElementById("countDisplay").innerHTML = count
+}
