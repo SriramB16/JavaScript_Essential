919004b3d95f9610e5d37d7e9879938ae7ec462d
SriramB16 <sridhanu2004@gmail.com>
2024-09-04 04:48:54 -0400

    pratice task finsihed

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 }
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python'},
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'Java' }
   ];
 
 function displayEmployees() {
@@ -34,4 +34,10 @@ function findEmployeeById(employeeId) {
       else{
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
       }
+    }
+
+    function bySpecialization() {
+        const specializations = employees.filter(employee => employee.specialization === 'Javascript');
+        const specialDisplay = specializations.map((employee,index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
+        document.getElementById('employeesDetails').innerHTML = specialDisplay;
     }
