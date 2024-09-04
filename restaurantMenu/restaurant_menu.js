e158221ae1d33cae1a14138fef5ede14befcc8a6
SriramB16 <sridhanu2004@gmail.com>
2024-09-04 04:02:09 -0400

    Javascript code has been added

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,17 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item,index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item,index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for(let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p> Item ${i + 1}: ${dessertMenu[i]}</p>`;
+};
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
