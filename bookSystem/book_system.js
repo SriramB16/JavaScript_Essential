013aa60ae467a96a6ff8d1c9ad10b421a2354aa6
SriramB16 <sridhanu2004@gmail.com>
2024-09-04 12:57:34 -0400

    practice task finished

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,8 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -35,4 +36,10 @@ function showbooks() {
             document.getElementById('authorName').value = '';
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
+ }
+
+
+ function deleteBook(index) {
+    books.splice(index,1);
+    showbooks();
  }
