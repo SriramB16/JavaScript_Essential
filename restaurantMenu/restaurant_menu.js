966759e862c8e6cbc218dbdc660aebb4f4a944cf
SriramB16 <sridhanu2004@gmail.com>
2024-09-04 04:04:33 -0400

    new changes made to array of Breakfast items

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,11 +1,11 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
-const breakfastMenuItemsHTML = breakfastMenu.map((item,index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+const breakfastMenuItemsHTML = breakfastMenu.map((item,index,) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
-let mainCourseItem = '';
+let mainCourseItem = ''
 mainCourseMenu.forEach((item,index) => {
 mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
 document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
