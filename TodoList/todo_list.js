276c18e9e8ec059fc7483290f04e1f75d0a344d5
SriramB16 <sridhanu2004@gmail.com>
2024-09-06 13:18:41 -0400

    todo list folder is added

---
+++ TodoList/todo_list.js
@@ -0,0 +1,44 @@
+const taskInput = document.getElementById("taskInput");
+
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+let tasks = [];
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        tasks.push({ text: taskText});
+        taskInput.value = "";
+        displayTasks();
+    }
+}
+
+function displayTasks() {
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+            <label for="task-${index}">${task.text}</label>`;
+        li.querySelector("input").addEventListener("change", () => toggleTask(index));
+        taskList.appendChild(li);
+    });
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    displayTasks();
+}
+
+
+function clearCompletedTasks() {
+    tasks = tasks.filter(task => !task.completed);
+    displayTasks();
+}
+
+addTaskBtn.addEventListener("click", addTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+
+
+displayTasks();
